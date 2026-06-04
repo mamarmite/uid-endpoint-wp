7146d1b7a545c113308b1ac1c22fded90a962ffd
Marc-André Martin <marcandre@mamarmite.com>
2026-06-04 14:32:16 -0400

    Version 0.3.0 vendor (PSR-4 principalement) ajout de l'autoload.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mamarmite/uid-endpoint',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1b151e9c2cae9c0ac1d18aee7550ca0cc2986fb1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mamarmite/uid-endpoint' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1b151e9c2cae9c0ac1d18aee7550ca0cc2986fb1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
