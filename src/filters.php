233fbf360ac4d3e7ee751ff7a56e525e738583eb
Marc-André Martin <marcandre@mamarmite.com>
2026-06-15 17:12:14 -0400

    Ajout des json d'ACF pour les schemas que le plugin utilise (auparavant dans le thème pendant le dev).

---
+++ src/filters.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mamarmite\UIDEndpoint;
+
+if (!defined('ABSPATH')) {
+    die('Invalid request.');
+}
+
+function add_uids_to_acf_load_point( $paths ) {
+    //we keep all other load point, but add ours.
+    // Append the new path and return it.
+    if (is_array($paths)) {
+        $paths[] = MAMARMITE_UID_ENDPOINT_BASE_PATH . '/acf-json';
+    }
+    return $paths;
+}
+add_filter( 'acf/settings/load_json', __NAMESPACE__.'\\add_uids_to_acf_load_point' );
